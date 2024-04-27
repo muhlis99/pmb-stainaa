a20c718dd498ba40e04f332d78666fd0163ffcc2
Ridlo21 <inurrie99@gmail.com>
2024-04-28 03:31:02 +0700

    pembayaran

--- frontend-pmb/src/components/Navbar.jsx
+++ frontend-pmb/src/components/Navbar.jsx
@@ -86,139 +86,31 @@ const Navbar = () => {
                 <div className="container px-0">
                     <div className="collapse navbar-collapse" id="navbar-default">
                         <ul className="navbar-nav">
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" id="navbarDashboard" data-bs-toggle="dropdown"
-                                    aria-haspopup="true" aria-expanded="false" data-bs-display="static">Dashboard</a>
-                                <ul className="dropdown-menu dropdown-menu-arrow" aria-labelledby="navbarDashboard">
-                                    <li>
-                                        <a className="dropdown-item" href="../admin-dashboard.html">Overview</a>
-                                    </li>
-                                    <li>
-                                        <a className="dropdown-item" href="../dashboard-analytics.html">Analytics</a>
-                                    </li>
-                                </ul>
+                            <li className="nav-item">
+                                <Link to="/dashboard" className='nav-link'>
+                                    Dashboard
+                                </Link>
                             </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" id="navbarPages" data-bs-toggle="dropdown"
-                                    aria-haspopup="true" aria-expanded="false">Pages</a>
-                                <ul className="dropdown-menu" aria-labelledby="navbarPages">
-                                    <li className="dropdown-submenu dropend">
-                                        <a className="dropdown-item dropdown-list-group-item dropdown-toggle"
-                                            href="#">Courses</a>
-                                        <ul className="dropdown-menu">
-                                            <li>
-                                                <a href="../admin-course-overview.html" className="dropdown-item">All
-                                                    Courses</a>
-                                            </li>
-                                            <li>
-                                                <a href="../admin-course-category.html" className="dropdown-item">Course
-                                                    Category</a>
-                                            </li>
-                                            <li>
-                                                <a href="../admin-course-category-single.html"
-                                                    className="dropdown-item">Category Single</a>
-                                            </li>
-                                        </ul>
-                                    </li>
-
-                                    <li className="dropdown-submenu dropend">
-                                        <a className="dropdown-item dropdown-list-group-item dropdown-toggle" href="#">Users</a>
-                                        <ul className="dropdown-menu">
-                                            <li>
-                                                <a href="../admin-instructor.html" className="dropdown-item">Instructor</a>
-                                            </li>
-                                            <li>
-                                                <a href="../admin-students.html" className="dropdown-item">Students</a>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                    <li className="dropdown-submenu dropend">
-                                        <a className="dropdown-item dropdown-list-group-item dropdown-toggle" href="#">CMS</a>
-                                        <ul className="dropdown-menu">
-                                            <li>
-                                                <a href="../admin-cms-overview.html" className="dropdown-item">Overview</a>
-                                            </li>
-                                            <li>
-                                                <a href="../admin-cms-post.html" className="dropdown-item">All Post</a>
-                                            </li>
-                                            <li>
-                                                <a href="../admin-cms-post-new.html" className="dropdown-item">New Post</a>
-                                            </li>
-                                            <li>
-                                                <a href="../admin-cms-post-category.html" className="dropdown-item">Category</a>
-                                            </li>
-                                        </ul>
-                                    </li>
-
-                                    <li className="dropdown-submenu dropend">
-                                        <a className="dropdown-item dropdown-list-group-item dropdown-toggle"
-                                            href="#">Project</a>
-                                        <ul className="dropdown-menu">
-                                            <li>
-                                                <a className="dropdown-item" href="../project-grid.html">Grid</a>
-                                            </li>
-                                            <li>
-                                                <a className="dropdown-item" href="../project-list.html">List</a>
-                                            </li>
-
-                                            <li className="dropdown-submenu dropend">
-                                                <a className="dropdown-item dropdown-list-group-item dropdown-toggle"
-                                                    href="#">Single</a>
-                                                <ul className="dropdown-menu">
-                                                    <li>
-                                                        <a className="dropdown-item"
-                                                            href="../project-overview.html">Overview</a>
-                                                    </li>
-                                                    <li>
-                                                        <a className="dropdown-item" href="../project-task.html">Task</a>
-                                                    </li>
-                                                    <li>
-                                                        <a className="dropdown-item" href="../project-budget.html">Budget</a>
-                                                    </li>
-                                                    <li>
-                                                        <a className="dropdown-item" href="../project-team.html">Team</a>
-                                                    </li>
-                                                    <li>
-                                                        <a className="dropdown-item" href="../project-files.html">Files</a>
-                                                    </li>
-                                                    <li>
-                                                        <a className="dropdown-item" href="../project-summary.html">Summary</a>
-                                                    </li>
-                                                </ul>
-                                            </li>
-                                            <li>
-                                                <a className="dropdown-item" href="../add-project.html">Create Project</a>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                    <li className="dropdown-submenu dropend">
-                                        <a className="dropdown-item dropdown-list-group-item dropdown-toggle" href="#">Site
-                                            Setting</a>
-                                        <ul className="dropdown-menu">
-                                            <li>
-                                                <a className="dropdown-item" href="../setting-general.html">General</a>
-                                            </li>
-                                            <li>
-                                                <a className="dropdown-item" href="../setting-google.html">google</a>
-                                            </li>
-                                            <li>
-                                                <a className="dropdown-item" href="../setting-social.html">Social</a>
-                                            </li>
-                                            <li>
-                                                <a className="dropdown-item" href="../setting-social-login.html">Social
-                                                    Login</a>
-                                            </li>
-                                            <li>
-                                                <a className="dropdown-item" href="../setting-payment.html">Payment</a>
-                                            </li>
-                                            <li>
-                                                <a className="dropdown-item" href="../setting-smpt.html">SMPT</a>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                </ul>
+                            <li className="nav-item">
+                                <Link to="/cekdetaildata" className='nav-link'>
+                                    Formulir
+                                </Link>
                             </li>
-
+                            <li className="nav-item">
+                                <Link to="/pembayaran" className='nav-link'>
+                                    Pembayaran
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className='nav-link'>
+                                    Seleksi
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className='nav-link'>
+                                    Informasi
+                                </Link>
+                            </li>
                         </ul>
                     </div>
                 </div>
---
+++ frontend-pmb/src/pages/user/formulir/CekFormulir.jsx
@@ -0,0 +1,161 @@
+import React, { useEffect, useState } from 'react'
+import LayoutUser from '../../LayoutUser'
+import { useDispatch, useSelector } from "react-redux"
+import { getMe } from "../../../features/authSlice"
+import { useNavigate, Link } from 'react-router-dom'
+import axios from 'axios'
+
+const CekFormulir = () => {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const { isError, user } = useSelector((state) => state.auth)
+    const [diri, setDiri] = useState("")
+    const [alamat, setAlamat] = useState("")
+    const [ortu, setOrtu] = useState("")
+    const [wali, setWali] = useState("")
+    const [berkas, setBerkas] = useState("")
+    const [token, setToken] = useState("")
+    const [fotoDiri, setFotoDiri] = useState("")
+    const [prevFoto, setPrevFoto] = useState("")
+    const [nama, setNama] = useState("")
+
+    useEffect(() => {
+        if (isError) {
+            navigate('/login')
+        }
+    }, [isError])
+
+    useEffect(() => {
+        dispatch(getMe())
+    }, [dispatch])
+
+    useEffect(() => {
+        getDetailForm()
+        if (user) {
+            setToken(user.data.token)
+        }
+    }, [user])
+
+    useEffect(() => {
+        lihatFoto()
+    }, [fotoDiri])
+
+    const getDetailForm = async () => {
+        try {
+            if (user) {
+                const response = await axios.get(`v1/formulir/checkByToken/${user.data.token}`)
+                setDiri(response.data.data[0].diri)
+                setAlamat(response.data.data[0].alamat)
+                setOrtu(response.data.data[0].ortu)
+                setWali(response.data.data[0].wali)
+                setBerkas(response.data.data[0].berkas)
+                setFotoDiri(response.data.data[0].foto)
+                setFotoDiri(response.data.data[0].foto)
+            }
+        } catch (error) {
+
+        }
+    }
+
+    const lihatFoto = async () => {
+        try {
+            if (fotoDiri) {
+                await axios.get(`v1/formulir/seeImage/pmb/diri/${fotoDiri} `, {
+                    responseType: "arraybuffer"
+                }).then((response) => {
+                    const base64 = btoa(
+                        new Uint8Array(response.data).reduce(
+                            (data, byte) => data + String.fromCharCode(byte),
+                            ''
+                        )
+                    )
+                    setPrevFoto(`data: ; base64, ${base64} `)
+                })
+
+            }
+        } catch (error) {
+
+        }
+    }
+
+    return (
+        <LayoutUser>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12 col-md-12 col-12">
+                        <div className="border-bottom pb-2 mb-3 d-lg-flex justify-content-between align-items-center">
+                            <div className="mb-2 mb-lg-0">
+                                <h1 className="mb-0 h2 fw-bold">Formulir</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="card">
+                            <div className="card-body">
+                                <div className="row">
+                                    <div className="col-md-12 text-center">
+                                        <h5>Mohon menyelesaikan pengisian formulir meliputi detail diri, detail alamat, detail orang tua sesuai kartu keluarga atau KTP.</h5>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-md-4">
+                                        <div className='row'>
+                                            <div className="col-sm-12">
+                                                <img src={prevFoto} className="img-thumbnail" alt="..." />
+                                            </div>
+                                        </div>
+                                        <div className="row">
+                                            <div className="col-md-12">
+                                                <h3>{nama}</h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-8 border-start border-dark">
+                                        <table cellPadding={5}>
+                                            <tbody>
+                                                <tr>
+                                                    <td><h5>Detail Diri</h5></td>
+                                                    <td>&nbsp;:&nbsp;</td>
+                                                    <td>{diri == 1 ? <h5 className='text-success'>Selesai</h5> : <h5 className="text-danger">Belum</h5>}</td>
+                                                    <td><Link to='/formulir1' state={{ token: token }} className={`btn btn-sm ${diri == 0 ? 'btn-success' : 'btn-info'} ms-3`}>{diri == 0 ? 'Mulai' : 'Lihat'}</Link></td>
+                                                </tr>
+                                                <tr>
+                                                    <td><h5>Detail Alamat</h5></td>
+                                                    <td>&nbsp;:&nbsp;</td>
+                                                    <td>{alamat == 1 ? <h5 className='text-success'>Selesai</h5> : <h5 className="text-danger">Belum</h5>}</td>
+                                                    <td><Link to='/formulir2' state={{ token: token }} className="btn btn-sm btn-info ms-3">{alamat == 0 ? 'Mulai' : 'Lihat'}</Link></td>
+                                                </tr>
+                                                <tr>
+                                                    <td><h5>Detail Orang Tua</h5></td>
+                                                    <td>&nbsp;:&nbsp;</td>
+                                                    <td>{ortu == 1 ? <h5 className='text-success'>Selesai</h5> : <h5 className="text-danger">Belum</h5>}</td>
+                                                    <td><Link to="/formulir3" state={{ token: token }} className="btn btn-sm btn-info ms-3">Mulai</Link></td>
+                                                </tr>
+                                                <tr>
+                                                    <td><h5>Detail Wali</h5></td>
+                                                    <td>&nbsp;:&nbsp;</td>
+                                                    <td>{wali == 1 ? <h5 className='text-success'>Selesai</h5> : <h5 className="text-danger">Belum</h5>}</td>
+                                                    <td><Link to="/formulir4" state={{ token: token }} className="btn btn-sm btn-info ms-3">Mulai</Link></td>
+                                                </tr>
+                                                <tr>
+                                                    <td><h5>Detail Berkas</h5></td>
+                                                    <td>&nbsp;:&nbsp;</td>
+                                                    <td>{berkas == 1 ? <h5 className='text-success'>Selesai</h5> : <h5 className="text-danger">Belum</h5>}</td>
+                                                    <td><Link to="/formulir5" state={{ token: token }} className="btn btn-sm btn-info ms-3">Mulai</Link></td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </LayoutUser>
+    )
+}
+
+export default CekFormulir
