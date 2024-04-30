ed2214a73334b39c76c45daa17ab8dc8570d3658
oles <muklek99@gmail.com>
2024-04-30 22:54:23 +0700

    approve

--- backend/controller/Capprove.js
+++ backend/controller/Capprove.js
@@ -4,6 +4,7 @@ const Mmahasiswa = require('../model/mahasiswaModel.js')
 const MhistoryMhs = require('../model/historyMahasiswaModel.js')
 const Mprodi = require('../model/Mprodi.js')
 const Msemester = require('../model/semesterModel.js')
+const MloginMhs = require('../model/loginModel.js')
 const {Sequelize,Op} =  require('sequelize')
 const argon = require('argon2')
 const nodemailer = require('nodemailer')
@@ -308,6 +309,16 @@ module.exports = {
             status: "aktif",
         })
 
+        const hashPassword = await argon.hash(nim)
+        await MloginMhs.create({
+            username: nim,
+            email: dataMhs.email,
+            password: hashPassword,
+            role: "mahasiswa",
+            verify_code: "",
+            status: "aktif"
+        })
+
         await MhistoryMhs.create({
             code_history: randomNumber +  dataSemester.code_semester,
             nim: nim,
@@ -354,68 +365,6 @@ module.exports = {
             })
         }).catch(err => {
             console.log(err);
-        })
-
-
-    },
-    
-    daftar : async (req, res, next) => {
-        const {nama, email, pass, conPass} = req.body
-        if (pass != conPass) return res.status(400).json({ message: "confirmasi password yang anda masukkan salah" })
-        const hashPass =  await argon.hash(pass)
-        await Mapprove.create({
-            nama : nama,
-            email : email,
-            password : hashPass,
-            role : "user",
-            verivikasi_kode : "",
-            status : "aktif"
-        }).then(async result => {
-            let randomNumber = Math.floor(100000 + Math.random() * 900000)
-            await Mapprove.update({
-                verifikasi_kode : randomNumber
-            }, {
-                where : {
-                    id_pmb : result.id_pmb
-                }
-            })
-            let testAccount = await nodemailer.createTestAccount()
-            let transporter = nodemailer.createTransport({
-                service: "gmail",
-                auth: {
-                    user: "muhammadbwi13@gmail.com",
-                    pass: "xzhltcpsznbllacw",
-                }
-            })
-            await transporter.sendMail({
-                from: 'muhammadbwi13@gmail.com',
-                to: `${email}`,
-                subject: "Atur Ulang Kata Kunci Apliaksi Mahasiswa STAINAA",
-                text: 'jangan disebarakan pada orang lain',
-                html: `<div class="card" style="width: 60%;>
-                            <div class="card-body">
-                                <h5 class="card-title">Tinggal satu langkah lagi
-                                Silakan masukkan kode verifikasi di bawah ini, untuk melanjutkan pendaftaran PMB STAINAA
-                                Kode ini jangan diberikan kepada siapapun.</h5>
-                                <h3 style="color:blue" class="card-text">${randomNumber}</h3>
-                                <h6><br>
-                                1. Kode ini dikirim oleh server PMB STAINAA.<br>
-                                2. Abaikan jika anda merasa tidak melakukannya.<br>
-                                3. Pesan ini dikirim menggunakan email otomatis, mohon untuk tidak membalas email ini.</h6>
-                                <hr>
-                                <h6>banyuwangi, ${new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')} WIB <br>
-                                Sekolah Tinggi Agama Islam Nurul Abror Al-Robbaniyin <br>
-                                jl. KH Agus Salim No 165 68453 email : muhlis_ganteng@gmail.com</h6>
-                            </div>
-                        </div>`
-            })
-            res.status(201).json({
-                message: "Data Mapprove Ditemukan",
-                data: result
-            })
-        }).
-        catch(err => {
-            next(err)
         })
     }
 }
